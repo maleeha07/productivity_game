d676b75f91b7f0e9c6666df4af4cab0b2455865e
maleeha07 <maleehaswalih@gmail.com>
2026-02-13 22:42:04 +0530

    test

--- lib/page/rewards_page.dart
+++ lib/page/rewards_page.dart
@@ -9,7 +9,7 @@ class RewardsPage extends StatelessWidget {
     int unlockedGifts = UserData.coins ~/ 100;
 
     List<String> gifts = [
-      "🎧 Headphones",
+      "🎧 Headset",
       "📚 New Book",
       "🍕 Free Treat",
       "🎮 Game Time",
